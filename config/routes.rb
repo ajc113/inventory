d09056e5681e6348beb9f473ae0d4c686a02b698
gullariz12 <gullariz82@gmail.com>
2023-07-20 21:59:41 +0500

    Added locations CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   end
 
   resources :sales
+  resources :locations
   resource :system_configuration, only: %i[show edit update]
 
   get "up" => "rails/health#show", as: :rails_health_check
