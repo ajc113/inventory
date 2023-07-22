b6ed92773d9831aa19852080bea1d235b4b9a33c
gullariz12 <gullariz82@gmail.com>
2023-07-22 09:20:48 +0500

    addded from method and report_recipients to configurations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_07_21_173706) do
+ActiveRecord::Schema[7.1].define(version: 2023_07_22_040723) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,6 +80,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_07_21_173706) do
     t.integer "alerting_quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "report_recipients", default: [], array: true
   end
 
   create_table "users", force: :cascade do |t|
