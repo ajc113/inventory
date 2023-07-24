22ba3cbfee95110ea94d91ab124da3256181c54a
gullariz12 <gullariz82@gmail.com>
2023-07-25 00:44:25 +0500

    more enhancements

--- app/javascript/controllers/nav_bar_controller.js
+++ app/javascript/controllers/nav_bar_controller.js
@@ -3,6 +3,24 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ['salesLocationsList', 'productionsLocationsList']
 
+  connect() {
+    let currentPath = window.location.pathname;
+
+    document.querySelectorAll('.nav-link').forEach( link => {
+      link.parentElement.classList.remove('active-link')
+    });
+
+    document.querySelector(`a[href="${currentPath}"]`).parentElement.classList.add('active-link');
+  }
+
+  activeLink(event) {
+    document.querySelectorAll('.nav-link').forEach( link => {
+      link.parentElement.classList.remove('active-link')
+    });
+
+    event.target.parentElement.classList.add('active-link');
+  }
+
   toggleLocationDropdown() {
     this.salesLocationsListTarget.classList.toggle('hidden');
   }
