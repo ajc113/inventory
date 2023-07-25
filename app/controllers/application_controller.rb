34b28fb7ba0d54f8e01703546afec121831a77a1
gullariz12 <gullariz82@gmail.com>
2023-07-25 16:02:25 +0500

    Added some fixes for the white background bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
+  include NavBar
   include Authenticate
   include Authorization
   include ExceptionHandler
+
+  before_action :set_stores
+  before_action :set_inventories
 end
