c644af619616af9ce89df2a83c2e3b8dfefeac75
gullariz12 <gullariz82@gmail.com>
2023-07-31 20:54:13 +0500

    Removed flavors drag and drop on Dashboard

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,7 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "sortable"
-pin "sortablejs"
 pin "chartkick", to: "chartkick.js"
 pin "Chart.bundle", to: "Chart.bundle.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
